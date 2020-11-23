231f0598f03adfca734794365ccf01b451a003b4
Ubuntu <ubuntu@ip-172-31-67-238.ec2.internal>
2020-11-23 18:32:06 +0000

    add avatar on Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+    
+    has_one_attached :avatar
+    
   validates_presence_of :name
   validates_presence_of :age
-    validates_presence_of     :email # optional
 end
